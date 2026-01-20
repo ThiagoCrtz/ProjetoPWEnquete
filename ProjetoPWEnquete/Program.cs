6576a4c72cbc92dfd105460a2771d8cf7dc47c46
Thiago Cortez <thiagopcortez@gmail.com>
2026-01-20 18:43:50 -0300

    alterando o connection string para o banco de dados do servidor

--- ProjetoPWEnquete/Program.cs
+++ ProjetoPWEnquete/Program.cs
@@ -5,43 +5,56 @@ using ProjetoPWEnquete.Services;
 using UserRoles.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
 );
-// Add services to the container.
+
+// MVC
 builder.Services.AddControllersWithViews();
+
+// Identity
 builder.Services.AddIdentity<User, IdentityRole>(options =>
 {
     options.Password.RequireDigit = true;
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = true;
-    options.Password.RequiredLength= 5;
+    options.Password.RequiredLength = 5;
+
     options.User.RequireUniqueEmail = true;
+
     options.SignIn.RequireConfirmedAccount = false;
     options.SignIn.RequireConfirmedEmail = false;
     options.SignIn.RequireConfirmedPhoneNumber = false;
-
 })
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
 
 var app = builder.Build();
-await SeedService.SeedDatabase(app.Services); 
-// Configure the HTTP request pipeline.
+
+// Seed
+await SeedService.SeedDatabase(app.Services);
+
+// Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // ? CORRETO PARA .NET 8
+
 app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
+// Rotas
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
